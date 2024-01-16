7d480068cd69a9482180134d8da433926d5892bf
dreaca <buddhikadreaca@gmail.com>
2024-01-16 21:44:39 +0530

    Initial commit

---
+++ src/main/java/com/example/chatapp/controller/Server.java
@@ -0,0 +1,21 @@
+package com.example.chatapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Server {
+
+    @FXML
+    private TextArea txtArea;
+
+    @FXML
+    private TextField txtSend;
+
+    @FXML
+    void sendOnAction(ActionEvent event) {
+
+    }
+
+}
